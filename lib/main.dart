9f21f059a33788806af2aebf16a53eced7e46456
Braian Manrique <braianm@iadb.org>
2024-07-20 12:38:59 -0500

    add home page

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget{
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true
       ),
-      // home: HomePage(),
+      home: HomePage(),
       // initialRoute: LoginPage.routename,
       // routes: {
       //   // LoginPage.routename  :(context) => const LoginPage(),
